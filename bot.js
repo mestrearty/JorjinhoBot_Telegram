897d4458ab4463a5ce3c66f833d7991cd0ec8522
Pedro Ventura <phventurarodrigues@gmail.com>
2020-04-08 23:55:32 -0300

    Usando versão antiga

--- bot.js
+++ bot.js
@@ -1,63 +1,57 @@
 const token = require("./.env");
-
 const Telegraf = require("telegraf");
 
-const bot = new Telegraf(env.token);
+const bot = new Telegraf(token);
+module.exports = function () {
+  bot.start((ctx) => {
+    const from = ctx.update.message.from;
+    ctx.reply(`Seja bem vindo, ${from.first_name}: ${from}`);
+  });
 
-const env = {
-  token,
-  apiUrl: `https:api.telegram.org/bot${token}`,
-  apiFileUrl: `https://api.telegram.org/file/bot${token}`,
-};
+  // Listen for any kind of message. There are different kinds of
+  // messages.
+  bot.on("message", (context) => {
+    if (context.update.message.text) {
+      const msg = context.update.message.text.toLowerCase();
 
-bot.start((ctx) => {
-  const from = ctx.update.message.from;
-  ctx.reply(`Seja bem vindo, ${from.first_name}: ${from}`);
-});
+      try {
+        if (msg.includes("onde")) {
+          console.log("no busão");
+          context.reply("No busão");
+        }
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on("message", (context) => {
-  if (context.update.message.text) {
-    const msg = context.update.message.text.toLowerCase();
+        if (msg.includes("bom")) {
+          context.reply("Bom é chupar um saco");
+        }
 
-    try {
-      if (msg.includes("onde")) {
-        console.log("no busão");
-        context.reply("No busão");
+        if (msg.includes("lista")) {
+          context.reply("Que lista????");
+        }
+
+        if (msg.includes("folha")) {
+          context.reply("Que folha?");
+        }
+
+        if (msg.includes("maquina") || msg.includes("máquina")) {
+          context.reply("vc quer uma máquina minha?");
+        }
+
+        if (msg.includes("laranja")) {
+          context.reply("você gosta de laranja?");
+        }
+
+        if (msg.includes("?")) {
+          context.reply("ta curioso?");
+        }
+
+        if (msg.includes("mew")) {
+          context.reply("mew meu com mew seu");
+        }
+      } catch (e) {
+        console.log(e);
       }
-
-      if (msg.includes("bom")) {
-        context.reply("Bom é chupar um saco");
-      }
-
-      if (msg.includes("lista")) {
-        context.reply("Que lista????");
-      }
-
-      if (msg.includes("folha")) {
-        context.reply("Que folha?");
-      }
-
-      if (msg.includes("maquina") || msg.includes("máquina")) {
-        context.reply("vc quer uma máquina minha?");
-      }
-
-      if (msg.includes("laranja")) {
-        context.reply("você gosta de laranja?");
-      }
-
-      if (msg.includes("?")) {
-        context.reply("ta curioso?");
-      }
-
-      if (msg.includes("mew")) {
-        context.reply("mew meu com mew seu");
-      }
-    } catch (e) {
-      console.log(e);
     }
-  }
-});
+  });
 
-bot.startPolling();
+  bot.startPolling();
+};
--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-'use strict';
+"use strict";
 
-var http = require('http');
-var port = (process.env.port || 5000);
+var http = require("http");
+var port = process.env.port || 5000;
 
-require('./bot')();
+require("./bot")();
 
-http.createServer(function(request,response){
-  response.writeHead(200,{'Content-Type': 'application/json'});
-  response.write(JSON.stringify({name: 'jorjinhobot',ver: '0.0.1'}));
-  response.end();
-}).listen(port);
-
-
-
-
-
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.write(JSON.stringify({ name: "jorjinhobot", ver: "0.0.1" }));
+    response.end();
+  })
+  .listen(port);
