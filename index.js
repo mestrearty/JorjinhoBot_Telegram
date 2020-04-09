2795fd0a7467d6787c31b25c7d324c68833ace71
Pedro Ventura <phventurarodrigues@gmail.com>
2020-04-08 23:35:03 -0300

    Bot

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
 var http = require('http');
 var port = (process.env.port || 5000);
 
+require('./bot')();
+
 http.createServer(function(request,response){
   response.writeHead(200,{'Content-Type': 'application/json'});
   response.write(JSON.stringify({name: 'jorjinhobot',ver: '0.0.1'}));
   response.end();
 }).listen(port);
+
+
+
 
 
